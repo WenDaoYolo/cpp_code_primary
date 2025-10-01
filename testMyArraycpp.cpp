0e448a7de1096e5841f17a201f029a9b6457cfc0
WenDaoYolo <3547474131@qq.com>
2025-10-01 21:26:16 +0800

    Add files via upload

---
+++ testMyArraycpp.cpp
@@ -0,0 +1,31 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include "MyArray.h"
+//
+//int main() {
+//	MyArray array1(10);
+//	for (int i = 0; i < 10; i++)
+//		array1.SetData(i,i * 10);
+//
+//	for (int i = 0; i < array1.GetLen(); i++)
+//		cout << array1.GetData(i) << " ";
+//	cout << endl;
+//	cout << "len1:" << array1.GetLen() << endl;;
+//
+//	cout << "------------------------" << endl;
+//
+//	MyArray array2(array1);
+//
+//	for (int i = 0; i < array2.GetLen(); i++)
+//		cout << array1.GetData(i) << " ";
+//	cout << endl;
+//	cout << "len2:" << array1.GetLen() << endl;;
+//
+//	/*       赋值操作符函数 -> 暂不了解，后续将学习
+//	
+//	MyArray array3;
+//	array3 = array2;
+//
+//	*/
+//
+//	return 0;
+//}
