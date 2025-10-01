36e86b743e6c9501f28415878d71628ef6238c69
WenDaoYolo <3547474131@qq.com>
2025-10-01 21:26:46 +0800

    Add files via upload

---
+++ MyArray.h
@@ -0,0 +1,19 @@
+//#pragma once
+//#include<iostream>
+//using namespace std;
+//
+//class MyArray
+//{
+//private:
+//	int len;
+//	int* space;
+//public:
+//	MyArray();
+//	MyArray(int len);
+//	MyArray(const MyArray& another);
+//	~MyArray();
+//	int GetLen();
+//	void SetData(int index, int data);
+//	int GetData(int index);
+//};
+//
