db476199278bb066b1997450b683412db59c52e0
WenDaoYolo <3547474131@qq.com>
2025-10-03 19:09:42 +0800

    Add files via upload

---
+++ c++_25.cpp
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+//class A {
+//private:
+//	int a;
+//public:
+//	friend void PrintBA(A& a1);
+//
+//};
+//
+//class B {
+//private:
+//	int b;
+//public:
+//	friend void PrintAB(B& b1);
+//
+//};
+//
+//void PrintBA(A& a1) {
+//	cout << a1.a << endl;
+//}
+//void PrintAB(B& b1) {
+//	cout << b1.b << endl;
+//}
+//
+//int main() {
+//	return 0;
+//}
+
+//class B;
+//class A;
+//
+//class B {
+//private:
+//	int b;
+//public:
+//	void PrintA(A& a1);
+//};
+//
+//class A {
+//private:
+//	int a;
+//public:
+//	friend void B::PrintA(A& a1);
+//};
+//
+//void B::PrintA(A& a1) {
+//	cout << a1.a << endl;;
+//}
+
+
+//class A {
+//private:
+//	int a;
+//public:
+//	friend void PrintBA(A& a1);
+//};
+//
+//class B {
+//private:
+//	int b;
+//public:
+//	friend void PrintAB(B& b1);
+//};
+//
+//void PrintBA(A& a1) {
+//	cout << a1.a << endl;
+//}
+//void PrintAB(B& b1) {
+//	cout << b1.b << endl;
+//}
+//
+//int main() {
+//
+//	return 0;
+//}
