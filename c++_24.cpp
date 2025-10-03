428b9f25aabd521ab6d129186c7b71b3347aeabf
WenDaoYolo <3547474131@qq.com>
2025-10-03 19:07:59 +0800

    Add files via upload

---
+++ c++_24.cpp
@@ -0,0 +1,63 @@
+#define _crt_secure_no_warnings 1
+#include<iostream>
+using namespace std;
+
+//class test {
+//private:
+//	int a;
+//public:
+//	test(int new_a) {
+//		this->a = new_a;
+//	}
+//
+//	friend void PrintA(test& t);         //声明该函数为test类的友元函数
+//};
+//
+//void PrintA(test& t) {
+//	cout << t.a << endl;
+//}
+//
+//int main() {
+//	test t1(10);
+//	test t2(20);
+//
+//	PrintA(t1);
+//	cout << "----------" << endl;
+//	PrintA(t2);
+//
+//	return 0;
+//}
+
+
+
+//class A {
+//private:
+//	int a;
+//public:
+//	friend void PrintBA(A a1);
+//	
+//};
+
+//class B {
+//private:
+//	int b;
+//public:
+//	friend void PrintAB(B b1);
+//	
+//};
+//
+//
+//void PrintBA(A a1) {
+//	cout << a1.a << endl;
+//}
+//void PrintAB(B b1) {
+//	cout << b1.b << endl;
+//}
+//
+
+
+
+
+
+
+
