a5b917222240d4b8ba69036be235ea325a5bb977
WenDaoYolo <3547474131@qq.com>
2025-10-08 20:52:35 +0800

    Add files via upload

---
+++ MyString.h
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#pragma once
+//#include<iostream>
+//using namespace std;
+//
+//class MyString
+//{
+//private:
+//	int len;
+//	char* str;
+//public:
+//	MyString();
+//	MyString(const char*);
+//	MyString(const MyString& another);
+//	~MyString();
+//
+//	MyString operator+(MyString another);
+//	MyString& operator=(MyString another);
+//	char& operator[](int index);
+//	bool operator==(MyString another);
+//	bool operator!=(MyString anotehr);
+//
+//	friend ostream& operator<<(ostream& os,MyString s);
+//    friend istream& operator>>(istream& os,MyString& s);
+//
+//};
+//
