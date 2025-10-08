92e08693f6da4d538145e53d72de949d8220424f
WenDaoYolo <3547474131@qq.com>
2025-10-08 20:53:25 +0800

    Add files via upload

---
+++ TestMyString.cpp
@@ -0,0 +1,30 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include "MyString.h"
+//
+//int main() {
+//	MyString s1;
+//	MyString s2("hello world!");
+//	MyString s3 = s2;
+//	MyString s4(s3);
+//	s1 = s2;
+//	MyString s7 = "hello";
+//	MyString s8 = "My Name is";
+//	MyString s9 = "Jack !";
+//	MyString s10;
+//	cin >> s10;
+//	cout << s2 << s3;
+//	cout << s1;
+//
+//	cout << s7 + s8 + s9 << endl;
+//	s10[0] = 'T';
+//	
+//	cout << s10 << endl;
+//	cout << s10[0] << endl;;
+//	
+//	if (s10 != s1)
+//		cout << "!=" << endl;
+//	if (s10 == s10)
+//		cout << "==" << endl;
+//
+//	return 0;
+//}
