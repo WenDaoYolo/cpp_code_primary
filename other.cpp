b7d9ed98cb0a99491bffaac00c9778f5114a363d
WenDaoYolo <3547474131@qq.com>
2025-10-13 21:22:43 +0800

    Add files via upload

---
+++ other.cpp
@@ -0,0 +1,15 @@
+#include "other.h"
+
+
+void PrintMenu(){
+    cout<<"=-------------------------------------------------------="<<endl;
+    cout<<"+                 [CONTACT SYSTEM]                      +"<<endl;
+    cout<<"+                                                       +"<<endl;
+    cout<<"+                 1.    ADD                             +"<<endl;                     
+    cout<<"+                 2.    DELETE                          +"<<endl;
+    cout<<"+                 3.    DISPLAY                         +"<<endl;
+    cout<<"+                 4.    SORT                            +"<<endl;
+    cout<<"+                 5.    LOOKUP                          +"<<endl;
+    cout<<"+                 6.    EXIT                            +"<<endl;
+    cout<<"=-------------------------------------------------------="<<endl;
+}
